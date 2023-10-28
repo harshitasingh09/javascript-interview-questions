435268e824e66972554b668c462b4d945eb618fb
harshita <harshita.singh@mcbirdtechnologies.com>
2023-10-28 12:36:26 +0530

    reversing string

--- String.js
+++ String.js
@@ -1,4 +1,4 @@
-
+// adding surname to name array
 const str = "Harshita"
 const arr = [{name: "Shivram"}, {name: "Harshita"}]
 const surname= [{surname:"kashyap"}, {surname: "Singh"}];
@@ -8,4 +8,22 @@ const surname= [{surname:"kashyap"}, {surname: "Singh"}];
 //  console.log(arr1);
  });
  
+ const combinedArray = arr.map((person, index) => {
+    return { name: `${person.name} ${surname[index].surname}` };
+  });
+
+
+  // reversing string 
+  const str1= "harshita";
+  let reversedStr='';
+  for(let i=str1.length-1;i>=0;i--)
+  {
+    reversedStr =reversedStr + str[i];
+  }
+console.log(reversedStr);
+
+// alternate way
+
+const result= str1.split('').reverse().join('');
+console.log(result);
  
